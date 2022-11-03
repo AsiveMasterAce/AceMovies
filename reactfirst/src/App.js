bd0592983f676b21e679368ade59f56156c7c101
AsiveMasterAce <asiveacemathambeka@gmail.com>
2022-11-03 14:46:47 +0200

    first commit

---
+++ reactfirst/src/App.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {useState,useEffect} from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+
+///9c232437
+
+const API_URL= 'http://www.omdbapi.com?apikey=9c232437';
+
+
+const movie1={
+  "Title": "Superman, Spiderman or Batman",
+  "Year": "2011",
+  "imdbID": "tt2084949",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
+
+const App=()=> {
+
+    const [movies, setMovies]=useState([]);
+    const [searchTerm,setSearchTerm]=useState('');
+
+    const searchMovies= async (title)=>{
+
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data= await response.json();
+
+      setMovies(data.Search);
+
+    }
+
+
+  useEffect(()=>{
+    searchMovies('Batman');
+  }, []);
+
+    return ( 
+    <div className = "app" >
+      
+       <h1>Ace_Movies </h1>
+       
+       <div className="search">
+        <input
+            placeholder="Search For Movies"
+            value={searchTerm}
+            onChange={(e)=> setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={()=>searchMovies(searchTerm)}
+        />
+        
+       </div>
+      { movies?.length>0
+        ?(
+          <div className='container'>
+          {movies.map((movie) =>(
+            <MovieCard movie={movie}/>
+          ))}
+           </div>
+        
+        ):
+        (
+          <div className='empty'>
+            <h2>No Movies Found</h2>
+
+          </div>
+        ) }
+      
+     </div>
+    );
+}
+
+export default App;
